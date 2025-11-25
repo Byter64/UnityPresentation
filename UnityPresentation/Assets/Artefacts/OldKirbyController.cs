1f05412e9ec128db869f8ba6249c61dc9184aac9
Byter <byter64@gmx.de>
2025-11-25 18:20:54 +0100

    Starting to add charector controller for kirby

--- UnityPresentation/Assets/Artefacts/OldKirbyController.cs
+++ UnityPresentation/Assets/Artefacts/OldKirbyController.cs
@@ -3,6 +3,9 @@ using UnityEngine.InputSystem;
 
 public class OldKirbyController : MonoBehaviour
 {
+	[SerializeField] private float walkSpeed;
+	[SerializeField] private float groundHeight;
+
 	private float horInput;
 	private bool duckInput;
 	private bool flyInput;
@@ -15,10 +18,44 @@ public class OldKirbyController : MonoBehaviour
 	private bool isSucking;
 	private bool isFull;
 	private bool isFlying;
+	private bool isJumping;
+
+	private Animator animator;
+
+	private void OnEnable()
+	{
+		animator = GetComponent<Animator>();
+	}
+
+	private void FixedUpdate()
+	{
+		horSpeed = horInput * walkSpeed;
+		isCrouching = duckInput;
+		isSucking = action2Input;
+		isFlying = flyInput;
+		isJumping = actionInput;
+
+
+		transform.position += new Vector3(horSpeed, 0, 0) * Time.fixedDeltaTime;
+
+		if(horInput != 0)
+		{
+			Vector3 scale = transform.localScale;
+			scale.x = Mathf.Abs(scale.x) * horInput;
+			transform.localScale = scale;
+		}
+
+		animator.SetFloat("horSpeed_Abs", Mathf.Abs(horSpeed));
+		animator.SetBool("isCrouching", isCrouching);
+		animator.SetBool("isSucking", isSucking);
+		animator.SetBool("isFull", isFull);
+		animator.SetBool("isFlying", isFlying);
+		animator.SetBool("isJumping", isJumping);
+	}
+
 
 	public void OnMove(InputAction.CallbackContext context)
 	{
-		if (!context.performed) return;
 		Vector2 input = context.ReadValue<Vector2>();
 		horInput = Mathf.RoundToInt(input.x);
 		duckInput = input.y < 0;
