5571ca5333d85d9db83cfe58ca18c6ad5d209c71
Byter <byter64@gmx.de>
2025-11-25 19:01:40 +0100

    Controller finished by 50%

--- UnityPresentation/Assets/Artefacts/OldKirbyController.cs
+++ UnityPresentation/Assets/Artefacts/OldKirbyController.cs
@@ -1,10 +1,15 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class OldKirbyController : MonoBehaviour
 {
 	[SerializeField] private float walkSpeed;
+	[SerializeField] private float fallSpeed;
 	[SerializeField] private float groundHeight;
+	[SerializeField] private float jumpHeight;
+	[SerializeField] private float jumpTime;
+	[SerializeField] private AnimationCurve jumpCurve;
 
 	private float horInput;
 	private bool duckInput;
@@ -19,6 +24,7 @@ public class OldKirbyController : MonoBehaviour
 	private bool isFull;
 	private bool isFlying;
 	private bool isJumping;
+	private bool isGrounded;
 
 	private Animator animator;
 
@@ -30,13 +36,22 @@ public class OldKirbyController : MonoBehaviour
 	private void FixedUpdate()
 	{
 		horSpeed = horInput * walkSpeed;
-		isCrouching = duckInput;
+		isCrouching = duckInput && isGrounded;
 		isSucking = action2Input;
 		isFlying = flyInput;
-		isJumping = actionInput;
+		isGrounded = transform.position.y == groundHeight;
 
+		if(!isGrounded && !isJumping && !isFlying)
+		{
+			Vector3 pos = transform.position;
+			pos.y -= fallSpeed * Time.fixedDeltaTime;
+			if(pos.y < groundHeight)
+				pos.y = groundHeight;
+			transform.position = pos;
+		}
 
-		transform.position += new Vector3(horSpeed, 0, 0) * Time.fixedDeltaTime;
+		if(!isCrouching)
+			transform.position += new Vector3(horSpeed, 0, 0) * Time.fixedDeltaTime;
 
 		if(horInput != 0)
 		{
@@ -51,6 +66,7 @@ public class OldKirbyController : MonoBehaviour
 		animator.SetBool("isFull", isFull);
 		animator.SetBool("isFlying", isFlying);
 		animator.SetBool("isJumping", isJumping);
+		animator.SetBool("isGrounded", isGrounded);
 	}
 
 
@@ -64,13 +80,31 @@ public class OldKirbyController : MonoBehaviour
 
 	public void OnAction(InputAction.CallbackContext context)
 	{
-		if (!context.performed) return;
-		actionInput = context.ReadValue<bool>();
+		actionInput = context.ReadValueAsButton();
+
+
+		if(context.performed && isGrounded)
+			StartCoroutine(Jump());
 	}
 
 	public void OnAction2(InputAction.CallbackContext context)
 	{
-		if (!context.performed) return;
-		action2Input = context.ReadValue<bool>();
+		action2Input = context.ReadValueAsButton();
+	}
+
+	private IEnumerator Jump()
+	{
+		isJumping = true;
+		float time = 0;
+		while(actionInput && time < jumpTime)
+		{
+			yield return new WaitForFixedUpdate();
+			time += Time.fixedDeltaTime;
+			Vector3 pos = transform.position;
+			pos.y = groundHeight + jumpCurve.Evaluate(time / jumpTime) * jumpHeight;
+			transform.position = pos;
+		}
+
+		isJumping = false;
 	}
 }
